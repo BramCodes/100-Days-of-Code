5afd7502657dd7223390722d85971acda1a01c57
Bram Pierik <Brampierik10@gmail.com>
2025-07-19 20:10:01 +0200

    Test

---
+++ Test.py
@@ -0,0 +1,3 @@
+# The code `print('Hallo world!')` is displaying the text "Hallo world!" on the screen. The `print()`
+# function in Python is used to output text or variables to the console.
+print('Hallo world!')
