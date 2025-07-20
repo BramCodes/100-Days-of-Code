30f9a5dae65fe4bba6db17313622d9bc58d3cc9c
Bram <153362732+BramCodes@users.noreply.github.com>
2025-07-20 13:39:14 +0200

    Delete Test.py

--- Test.py
+++
@@ -1,3 +0,0 @@
-# The code `print('Hallo world!')` is displaying the text "Hallo world!" on the screen. The `print()`
-# function in Python is used to output text or variables to the console.
-print('Hallo world!')
